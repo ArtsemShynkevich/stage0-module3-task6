467ab7801c7abfbb4751155f49f2e8ab61f754ae
artem_shinkevich <SKILLBOX2021oNe>
2022-11-14 23:15:30 +0300

    Done better!

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -10,9 +10,9 @@ public class OperatorPrecedence {
         boolean secondBool = false;
         boolean thirdBool = 1 == 1;
         boolean fourthBool = 4 <= 101;
-        int varSix = 17/(first+second);
+        int varSix = 17;
 
-        System.out.println(firstBool && (secondBool || thirdBool) && (fourthBool || (varSix = 5) < 0));
+        System.out.println(firstBool && ((secondBool || thirdBool)) && fourthBool || (varSix = 5) < 0);
         System.out.println(varSix);
     }
 }
